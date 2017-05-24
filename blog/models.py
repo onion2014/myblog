f02b5065f04c8f6865240dfd13d016a2451d3c14
tengyifeng <471571121@qq.com>
2017-05-24 18:10:22 +0800

    modify-5.24

--- blog/models.py
+++ blog/models.py
@@ -30,9 +30,14 @@ class Post(models.Model):
     # 给数据库增加字段
     title = models.CharField(max_length=70)
     body = models.TextField()
+    # 这两个列分别表示文章的创建时间和最后一次修改时间，存储时间的字段用 DateTimeField 类型。
     created_time = models.DateTimeField()
     modified_time = models.DateTimeField()
-    excerpt = models.CharField(max_length=200, blank=True)
+
+    # 文章摘要，可以没有文章摘要，但默认情况下 CharField 要求我们必须存入数据，否则就会报错。
+    # 指定 CharField 的 blank=True 参数值后就可以允许空值了。
+    excerpt = models.CharField(max_length=200)
+
     category = models.ForeignKey(Category)
     tags = models.ManyToManyField(Tag, blank=True)
     author = models.ForeignKey(User)
