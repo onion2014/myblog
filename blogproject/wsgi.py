e52d8fa70ce6f46269bdc605a6f2b00d62b32778
tengyifeng <471571121@qq.com>
2017-06-24 15:20:37 +0800

    change-wsgi-2

--- blogproject/wsgi.py
+++ blogproject/wsgi.py
@@ -11,11 +11,11 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import django.core
+import django.core.handlers
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogproject.settings")
-sys.path.append('/usr/lib/python2.7/site-packages')
-sys.path.append('/usr/lib64/python2.7/site-packages')
 
 application = get_wsgi_application()
