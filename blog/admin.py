098ded7e3f4e6aa1390d5ecd97a96b9ee469bb4f
tengyifeng <471571121@qq.com>
2017-07-11 22:55:42 +0800

    add kindeditor

--- blog/admin.py
+++ blog/admin.py
@@ -4,9 +4,17 @@
 from django.contrib import admin
 from .models import Post, Category, Tag
 
-
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
+
+    class Media:
+        # 在管理后台的HTML文件中加入js文件, 每一个路径都会追加STATIC_URL/
+        js = (
+            '/static/js/kindeditor/kindeditor-all-min.js',
+            '/static/js/kindeditor/lang/zh-CN.js',
+            '/static/js/kindeditor/config.js',
+        )
+
 
 # Register your models here.
 # 把新增的 PostAdmin 也注册进来
---
+++ blog/static/js/kindeditor/config.js
@@ -0,0 +1,8 @@
+//config.js
+KindEditor.ready(function(K) {
+    K.create('#id_body',{
+        // 指定大小
+        width:'680px',
+        height:'300px',
+    });
+});
